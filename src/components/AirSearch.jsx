90b7d3207115b5475a484476a9c41464b3f9424e
James Pesenti <you@example.com>
2022-03-08 12:13:01 -0500

    editFeedback button

---
+++ src/components/AirSearch.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from "./shared/Card"
+
+function AirSearch() {
+  return (
+      <Card className="airsearch"> 
+        <input className="airsearch" type="text" placeholder="search"/>
+        </Card>
+   
+  )
+}
+
+export default AirSearch
